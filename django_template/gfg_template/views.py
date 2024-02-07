c3684f152a046379fc0c4072ea88f003afe30034
yhmunna1 <yh.munna.barisal@gmail.com>
2024-02-07 20:04:55 +0600

    use template

--- django_template/gfg_template/views.py
+++ django_template/gfg_template/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    d = {'author' : "R'ahim",'fullname': 'Md Rahim Uddin' , 'father': 'malek', 'age': 20, 'brother': '', 'birthday': datetime.datetime.now(), 'size': '123456789', 'courses': [
+        {
+            'id': 1,
+            'name': "Python",
+            'fee': 5000
+        },
+        {
+            'id': 2,
+            'name': "Django",
+            'fee': 3000
+        },
+        {
+            'id': 3,
+            'name': "Javascript",
+            'fee': 5000
+        },
+    ]}
+    return render(request, 'home.html', d)
 
 def about(request):
     return render(request, 'about.html')
